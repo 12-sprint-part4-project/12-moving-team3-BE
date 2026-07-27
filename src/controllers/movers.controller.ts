f4f5a11dffd835d71f232b12cfeec506a400bd1b
narin116 <aasdff1010@gmail.com>
2026-07-27 15:36:01 +0900

    feat: getMoverDetail Controller 작성

--- src/controllers/movers.controller.ts
+++ src/controllers/movers.controller.ts
@@ -1,8 +1,34 @@
 import type { Request, Response, NextFunction } from 'express';
+import moversService from '../services/movers.service';
 
-export const getMovers = (req: Request, res: Response, next: NextFunction) => {
+export const getMovers = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
   try {
     const queryParams = req.query;
+    const movers = await moversService.getMovers(queryParams);
+
+    res.status(200).json({
+      data: movers,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const getMoverDetail = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const { id } = req.params;
+    const mover = await moversService.getMoverDetail(id);
+    res.status(200).json({
+      data: mover,
+    });
   } catch (error) {
     next(error);
   }
