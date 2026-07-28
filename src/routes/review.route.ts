572adcdb766cd8b2d3c5318d9920635143aa5fae
narin116 <aasdff1010@gmail.com>
2026-07-28 17:48:33 +0900

    feat: review Schema추가 및 validateRequest 작성

--- src/routes/review.route.ts
+++ src/routes/review.route.ts
@@ -1,28 +1,47 @@
 import { Router } from 'express';
 import { allowUserTypes, requireAuth } from '../middlewares/auth.middleware';
 import { validateRequest } from '../middlewares/validate.middleware';
+import { quoteIdParamsSchema } from '../schemas/quote.schema';
+import {
+  reviewBodySchema,
+  reviewIdParamsSchema,
+  reviewListQuerySchema,
+} from '../schemas/review.schema';
 
 const router = Router();
-
+//리뷰 등록
 router.post(
-  '/:estimateId',
+  '/:quoteId',
   requireAuth,
   allowUserTypes('CUSTOMER'),
   validateRequest({
+    params: quoteIdParamsSchema,
     body: reviewBodySchema,
     errorCode: 'INVALID_REQUEST',
   })
-); //리뷰 등록
+);
+//리뷰 수정
 router.patch(
   '/:reviewId',
   requireAuth,
   allowUserTypes('CUSTOMER'),
   validateRequest({
+    params: reviewIdParamsSchema,
     body: reviewBodySchema,
     errorCode: 'INVALID_REQUEST',
   })
-); //리뷰 수정
-router.delete('/:reviewId', requireAuth, allowUserTypes('CUSTOMER')); //리뷰 삭제 (소프트 딜리트)
+);
+//리뷰 삭제 (소프트 딜리트)
+router.delete(
+  '/:reviewId',
+  requireAuth,
+  allowUserTypes('CUSTOMER'),
+  validateRequest({
+    params: reviewIdParamsSchema,
+    errorCode: 'INVALID_REQUEST',
+  })
+);
+//기사님의 리뷰 목록 조회
 router.get(
   '/mover',
   requireAuth,
@@ -31,7 +50,8 @@ router.get(
     query: reviewListQuerySchema,
     errorCode: 'INVALID_QUERY_PARAM',
   })
-); //기사님의 리뷰 목록 조회
+);
+//고객의 리뷰 목록 조회
 router.get(
   '/customer',
   requireAuth,
@@ -40,7 +60,8 @@ router.get(
     query: reviewListQuerySchema,
     errorCode: 'INVALID_QUERY_PARAM',
   })
-); //고객의 리뷰 목록 조회
+);
+//리뷰 작성 가능한 견적 조회
 router.get(
   '/customer/writeable',
   requireAuth,
@@ -49,7 +70,7 @@ router.get(
     query: reviewListQuerySchema,
     errorCode: 'INVALID_QUERY_PARAM',
   })
-); //리뷰 작성 가능한 견적 조회
+);
 //리뷰 통계 정보 가져오기 (movers할 때 구현한..)
 
 export default router;
