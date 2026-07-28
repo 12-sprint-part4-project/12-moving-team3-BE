b090104eef1525284bb0152aead6db529e9f828d
narin116 <aasdff1010@gmail.com>
2026-07-28 17:50:39 +0900

    fix: router 순서 재배치

--- src/routes/review.route.ts
+++ src/routes/review.route.ts
@@ -9,6 +9,38 @@ import {
 } from '../schemas/review.schema';
 
 const router = Router();
+
+//기사님의 리뷰 목록 조회
+router.get(
+  '/mover',
+  requireAuth,
+  allowUserTypes('MOVER'),
+  validateRequest({
+    query: reviewListQuerySchema,
+    errorCode: 'INVALID_QUERY_PARAM',
+  })
+);
+//리뷰 작성 가능한 견적 조회
+router.get(
+  '/customer/writeable',
+  requireAuth,
+  allowUserTypes('CUSTOMER'),
+  validateRequest({
+    query: reviewListQuerySchema,
+    errorCode: 'INVALID_QUERY_PARAM',
+  })
+);
+//고객의 리뷰 목록 조회
+router.get(
+  '/customer',
+  requireAuth,
+  allowUserTypes('CUSTOMER'),
+  validateRequest({
+    query: reviewListQuerySchema,
+    errorCode: 'INVALID_QUERY_PARAM',
+  })
+);
+
 //리뷰 등록
 router.post(
   '/:quoteId',
@@ -41,36 +73,7 @@ router.delete(
     errorCode: 'INVALID_REQUEST',
   })
 );
-//기사님의 리뷰 목록 조회
-router.get(
-  '/mover',
-  requireAuth,
-  allowUserTypes('MOVER'),
-  validateRequest({
-    query: reviewListQuerySchema,
-    errorCode: 'INVALID_QUERY_PARAM',
-  })
-);
-//고객의 리뷰 목록 조회
-router.get(
-  '/customer',
-  requireAuth,
-  allowUserTypes('CUSTOMER'),
-  validateRequest({
-    query: reviewListQuerySchema,
-    errorCode: 'INVALID_QUERY_PARAM',
-  })
-);
-//리뷰 작성 가능한 견적 조회
-router.get(
-  '/customer/writeable',
-  requireAuth,
-  allowUserTypes('CUSTOMER'),
-  validateRequest({
-    query: reviewListQuerySchema,
-    errorCode: 'INVALID_QUERY_PARAM',
-  })
-);
+
 //리뷰 통계 정보 가져오기 (movers할 때 구현한..)
 
 export default router;
