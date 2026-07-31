276d3df4307a096e705833217dd7e470c7a233d5
gptjd0204 <gptjd0204@naver.com>
2026-07-31 15:46:12 +0900

    refactor: port 설정 env에 추가

--- src/app.ts
+++ src/app.ts
@@ -95,8 +95,8 @@ app.use('/api', presignedUrlRouter);
 
 app.use(errorHandler);
 
-app.listen(3000, () => {
-  console.log('Server is running on port 3000');
+app.listen(process.env.PORT, () => {
+  console.log(`Server is running on port ${process.env.PORT}`);
   if (process.env.NODE_ENV !== 'production') {
     console.log(`Swagger docs: ${SWAGGER_BASE_URL}/api-docs`);
   }
